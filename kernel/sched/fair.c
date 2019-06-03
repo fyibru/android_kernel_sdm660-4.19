4aa0534405f143d51f6b4b11cdc36372b235e467
Valentin Schneider <valentin.schneider@arm.com>
2019-06-03 12:54:24 +0100

    sched/fair: Clean up definition of NOHZ blocked load functions
    cfs_rq_has_blocked() and others_have_blocked() are only used within
    update_blocked_averages(). The !CONFIG_FAIR_GROUP_SCHED version of the
    latter calls them within a #define CONFIG_NO_HZ_COMMON block, whereas
    the CONFIG_FAIR_GROUP_SCHED one calls them unconditionnally.
    
    As reported by Qian, the above leads to this warning in
    !CONFIG_NO_HZ_COMMON configs:
    
      kernel/sched/fair.c: In function 'update_blocked_averages':
      kernel/sched/fair.c:7750:7: warning: variable 'done' set but not used [-Wunused-but-set-variable]
    
    It wouldn't be wrong to keep cfs_rq_has_blocked() and
    others_have_blocked() as they are, but since their only current use is
    to figure out when we can stop calling update_blocked_averages() on
    fully decayed NOHZ idle CPUs, we can give them a new definition for
    !CONFIG_NO_HZ_COMMON.
    
    Change the definition of cfs_rq_has_blocked() and
    others_have_blocked() for !CONFIG_NO_HZ_COMMON so that the
    NOHZ-specific blocks of update_blocked_averages() become no-ops and
    the 'done' variable gets optimised out.
    
    While at it, remove the CONFIG_NO_HZ_COMMON block from the
    !CONFIG_FAIR_GROUP_SCHED definition of update_blocked_averages() by
    using the newly-introduced update_blocked_load_status() helper.
    
    No change in functionality intended.
    
    [ Additions by Peter Zijlstra. ]
    
    Reported-by: Qian Cai <cai@lca.pw>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20190603115424.7951-1-valentin.schneider@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: I15b7052783257e7f2b5885977295b54ca6026985

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9522,32 +9522,6 @@ static inline bool others_have_blocked(struct rq *rq) { return false; }
 static inline void update_blocked_load_status(struct rq *rq, bool has_blocked) {}
 #endif
 
-static bool __update_blocked_others(struct rq *rq, bool *done)
-{
-	const struct sched_class *curr_class;
-	u64 now = rq_clock_pelt(rq);
-	unsigned long thermal_pressure;
-	bool decayed;
-
-	/*
-	 * update_load_avg() can call cpufreq_update_util(). Make sure that RT,
-	 * DL and IRQ signals have been updated before updating CFS.
-	 */
-	curr_class = rq->curr->sched_class;
-
-	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
-
-	decayed = update_rt_rq_load_avg(now, rq, curr_class == &rt_sched_class) |
-		  update_dl_rq_load_avg(now, rq, curr_class == &dl_sched_class) |
-		  update_thermal_load_avg(rq_clock_thermal(rq), rq, thermal_pressure) |
-		  update_irq_load_avg(rq, 0);
-
-	if (others_have_blocked(rq))
-		*done = false;
-
-	return decayed;
-}
-
 #ifdef CONFIG_FAIR_GROUP_SCHED
 
 static inline bool cfs_rq_is_decayed(struct cfs_rq *cfs_rq)
