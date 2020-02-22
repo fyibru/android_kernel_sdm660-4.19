6b21a0700fa6fe3aca54225f3f132d417848d047
Thara Gopinath <thara.gopinath@linaro.org>
2020-02-21 19:52:06 -0500

    sched/topology: Add callback to read per CPU thermal pressure
    Introduce the arch_scale_thermal_pressure() callback to retrieve per CPU thermal
    pressure.
    
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/20200222005213.3873-3-thara.gopinath@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -222,6 +222,15 @@ unsigned long arch_scale_cpu_capacity(int cpu)
 {
 	return SCHED_CAPACITY_SCALE;
 }
+
+#endif	/* !CONFIG_SMP */
+
+#ifndef arch_scale_thermal_pressure
+static __always_inline
+unsigned long arch_scale_thermal_pressure(int cpu)
+{
+	return 0;
+}
 #endif
 
 static inline int task_node(const struct task_struct *p)
