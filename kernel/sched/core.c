ee481314b05406d3537fcf48d09413e88d053c2d
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:42 +0200

    sched/fair: Add lag based placement
    With the introduction of avg_vruntime, it is possible to approximate
    lag (the entire purpose of introducing it in fact). Use this to do lag
    based placement over sleep+wake.
    
    Specifically, the FAIR_SLEEPERS thing places things too far to the
    left and messes up the deadline aspect of EEVDF.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124603.794929315@infradead.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3287,6 +3287,7 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	p->boost_period			= 0;
 	p->low_latency			= 0;
 #endif
+	p->se.vlag			= 0;
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
