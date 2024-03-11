d349e9ee73258800f953e9211c4ffd86d327e6d1
Cherry <workingn6@gmail.com>
2024-03-11 03:21:24 +0000

    Revert "6a2d77d: move pr_err message to stock"
    This reverts commit d31e0ceba8bbf9eb07e08be2e3767ab0faf48bf9.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -89,10 +89,11 @@ static int try_to_freeze_tasks(bool user_only)
 	elapsed_msecs = ktime_to_ms(elapsed);
 
 		if (todo) {
-		pr_err("Freezing of tasks failed after %d.%03d seconds"
-		       " (%d tasks refusing to freeze, wq_busy=%d):\n",
-		       elapsed_msecs / 1000, elapsed_msecs % 1000,
-		       todo - wq_busy, wq_busy);
++		pr_err("Freezing %s %s after %d.%03d seconds "
+ 		       "(%d tasks refusing to freeze, wq_busy=%d):\n", what,
++		       wakeup ? "aborted" : "failed",
+ 		       elapsed_msecs / 1000, elapsed_msecs % 1000,
+ 		       todo - wq_busy, wq_busy);
 
 		if (wq_busy)
 			show_workqueue_state();
