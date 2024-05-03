c2459e656ae94137e92a6a8687d32a10be95485c
ellenoireQ oz <ketchupnaiso@gmail.com>
2024-05-03 13:40:43 +0000

    loop: fix incompatible int because functiorg a voig a void function

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1617,7 +1617,9 @@ static int loop_set_capacity(struct loop_device *lo)
 	if (unlikely(lo->lo_state != Lo_bound))
 		return -ENXIO;
 
-	return figure_loop_size(lo, lo->lo_offset, lo->lo_sizelimit);
+	figure_loop_size(lo, lo->lo_offset, lo->lo_sizelimit);
+	
+	return 0;
 }
 
 static int loop_set_dio(struct loop_device *lo, unsigned long arg)
