c9c041ed337d0d95b5c6f9a13515b77ca4571d9e
ellenoireQ oz <ketchupnaiso@gmail.com>
2024-05-03 13:41:06 +0000

    Revert "loop: fix incompatible int because functiorg a voig a void function"
    This reverts commit c2459e656ae94137e92a6a8687d32a10be95485c.

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -1617,9 +1617,7 @@ static int loop_set_capacity(struct loop_device *lo)
 	if (unlikely(lo->lo_state != Lo_bound))
 		return -ENXIO;
 
-	figure_loop_size(lo, lo->lo_offset, lo->lo_sizelimit);
-	
-	return 0;
+	return figure_loop_size(lo, lo->lo_offset, lo->lo_sizelimit);
 }
 
 static int loop_set_dio(struct loop_device *lo, unsigned long arg)
