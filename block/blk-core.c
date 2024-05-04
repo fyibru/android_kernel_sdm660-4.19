57e17288dc747931c25f1121bd09f7babf4c6711
ellenoireQ oz <ketchupnaiso@gmail.com>
2024-05-04 03:12:15 +0000

    blk-core: silent annoying log

--- block/blk-core.c
+++ block/blk-core.c
@@ -2132,12 +2132,13 @@ out_unlock:
 static void handle_bad_sector(struct bio *bio, sector_t maxsector)
 {
 	char b[BDEVNAME_SIZE];
+	bool skip;
+	
+			if ((bio_devname(bio, b) && bio->bi_opf &&
+				(unsigned long long)bio_end_sector(bio) &&
+				(long long)maxsector))
+				skip = true;	
 
-	printk(KERN_INFO "attempt to access beyond end of device\n");
-	printk(KERN_INFO "%s: rw=%d, want=%Lu, limit=%Lu\n",
-			bio_devname(bio, b), bio->bi_opf,
-			(unsigned long long)bio_end_sector(bio),
-			(long long)maxsector);
 }
 
 #ifdef CONFIG_FAIL_MAKE_REQUEST
