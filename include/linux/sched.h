b4e2aa8b88474a6d73e7b8294cb7029e75ce00bd
ellenoireQ <166298239+ellenoireQ@users.noreply.github.com>
2024-05-30 13:59:18 +0000

    Revert "sched/fair: Add lag based placement"
    This reverts commit d923fda7ca5c041f728197a3ab309ff146268ca5.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -533,10 +533,8 @@ struct sched_entity {
 
 	u64				exec_start;
 	u64				sum_exec_runtime;
-	u64				prev_sum_exec_runtime;
 	u64				vruntime;
-	s64				vlag;
-
+	u64				prev_sum_exec_runtime;
 	u64				nr_migrations;
 
 	struct sched_statistics		statistics;
